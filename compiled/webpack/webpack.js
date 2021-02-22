928f8b9d704c55743dc84500fe7c6e21ccca528c
chencheng <sorrycc@gmail.com>
2021-02-22 17:04:52 +0800

    fix: webpack's main field and export format

--- compiled/webpack/webpack.js
+++ compiled/webpack/webpack.js
@@ -7,19 +7,20 @@ let initializedWebpack4 = false;
 let initFns = [];
 exports.init = function (useWebpack5) {
   if (useWebpack5) {
-    Object.assign(exports, require('./5/bundle5')());
+    Object.assign(exports, require('./5/bundle5')().webpack);
+    exports.isWebpack5 = true;
     exports.isWebpack5 = true;
     if (!initializedWebpack5) for (const cb of initFns) cb();
     initializedWebpack5 = true;
   } else {
-    Object.assign(exports, require('./4/bundle4')());
+    Object.assign(exports, require('./4/bundle4')().webpack);
     exports.isWebpack5 = false;
     if (!initializedWebpack4) for (const cb of initFns) cb();
     initializedWebpack4 = true;
   }
 }
 
-exports.onWebpackInit = function (cb) {
+exports.onWebpackInit = function(cb) {
   if (initializedWebpack5 || initializedWebpack4) cb();
   initFns.push(cb);
 }
