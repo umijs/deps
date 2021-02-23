4439325516a2ff4f7b334f5bc3895990bbb3f067
chencheng <sorrycc@gmail.com>
2021-02-23 11:27:54 +0800

    Add Compilation and RequestShortener for webpack

--- bundles/webpack/bundle4.js
+++ bundles/webpack/bundle4.js
@@ -2,11 +2,13 @@
 
 module.exports = function () {
   return {
+    Compilation: require('webpack/lib/Compilation'),
     LibraryTemplatePlugin: require('webpack/lib/LibraryTemplatePlugin'),
     LimitChunkCountPlugin: require('webpack/lib/optimize/LimitChunkCountPlugin'),
     NodeTargetPlugin: require('webpack/lib/node/NodeTargetPlugin'),
     NodeTemplatePlugin: require('webpack/lib/node/NodeTemplatePlugin'),
     NormalModule: require('webpack/lib/NormalModule'),
+    RequestShortener: require('webpack/lib/RequestShortener'),
     SingleEntryPlugin: require('webpack/lib/SingleEntryPlugin'),
     sources: require('webpack-sources'),
     webpack: require('webpack'),
--- bundles/webpack/bundle5.js
+++ bundles/webpack/bundle5.js
@@ -2,11 +2,13 @@
 
 module.exports = function () {
   return {
-    LibraryTemplatePlugin: require('webpack/lib/LibraryTemplatePlugin'),
-    LimitChunkCountPlugin: require('webpack/lib/optimize/LimitChunkCountPlugin'),
+    Compilation: require('webpack5/lib/Compilation'),
+    LibraryTemplatePlugin: require('webpack5/lib/LibraryTemplatePlugin'),
+    LimitChunkCountPlugin: require('webpack5/lib/optimize/LimitChunkCountPlugin'),
     NodeTargetPlugin: require('webpack5/lib/node/NodeTargetPlugin'),
-    NodeTemplatePlugin: require('webpack/lib/node/NodeTemplatePlugin'),
+    NodeTemplatePlugin: require('webpack5/lib/node/NodeTemplatePlugin'),
     NormalModule: require('webpack5/lib/NormalModule'),
+    RequestShortener: require('webpack5/lib/RequestShortener'),
     SingleEntryPlugin: require('webpack/lib/SingleEntryPlugin'),
     sources: require('webpack5').sources,
     webpack: require('webpack5'),
